1b21a28000331c01658ed29f552b18a3091e1174
Kelvin Kahuho <kelvin.kahuho@eloraholdings.com>
2025-02-17 17:04:17 +0300

    Printing gimics on the command line

---
+++ kelvin.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void print_banner() {
+    printf("\n");
+    printf(" KK   KK  EEEEE  LL      VV      VV  III  NN    NN \n");
+    printf(" KK  KK   EE     LL       VV    VV    I   NNN   NN \n");
+    printf(" KK KK    EEEE   LL        VV  VV     I   NN N  NN \n");
+    printf(" KK  KK   EE     LL         VVVV      I   NN  N NN \n");
+    printf(" KK   KK  EEEEE  LLLLLL      VV      III  NN   NNN \n");
+    printf("\n");
+}
+
+int main() {
+    print_banner();
+    printf("Welcome to Kelvin's Tool!\n");
+    return 0;
+}
