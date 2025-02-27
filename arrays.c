5627a2fdde6f8105d2ec610a73c14fa0194606fc
Kelvin Kahuho <kelvin.kahuho@eloraholding.com>
2025-02-27 16:59:15 +0300

    Array structure in C

---
+++ arrays.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    int arr[5] = {1, 2, 3, 4, 5};
+    printf("First element: %d\n", arr[0]);
+    printf("Second element: %d\n", arr[1]);
+    printf("Third element: %d\n", arr[2]);
+    printf("Fourth element: %d\n", arr[3]);
+    return 0;
+}
